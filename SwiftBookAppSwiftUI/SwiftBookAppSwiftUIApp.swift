de97f32ce61daf1b4523a5d639737661a63992f0
Alexey Turulin <repakuku@icloud.com>
2023-09-18 19:42:57 -0400

    Initial Commit

---
+++ SwiftBookAppSwiftUI/SwiftBookAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBookAppSwiftUIApp.swift
+//  SwiftBookAppSwiftUI
+//
+//  Created by Alexey Turulin on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBookAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
