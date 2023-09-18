6ae5e1b76aff77144e36d24b0a52e386de1e23de
Alexey Turulin <repakuku@icloud.com>
2023-09-18 19:56:50 -0400

    added network manager

---
+++ SwiftBookAppSwiftUI/Services/NetworkManager.swift
@@ -0,0 +1,22 @@
+//
+//  NetworkManager.swift
+//  SwiftBookAppSwiftUI
+//
+//  Created by Alexey Turulin on 9/18/23.
+//
+
+import Foundation
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetchData() {
+        
+    }
+    
+    func fetchImage() {
+        
+    }
+}
