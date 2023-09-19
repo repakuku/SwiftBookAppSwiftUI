bac6bf6e4efe62e56c12ad3e623d0450ec1a2967
Alexey Turulin <repakuku@icloud.com>
2023-09-18 20:06:26 -0400

    working with network manager

--- SwiftBookAppSwiftUI/Services/NetworkManager.swift
+++ SwiftBookAppSwiftUI/Services/NetworkManager.swift
@@ -10,13 +10,34 @@ import Foundation
 final class NetworkManager {
     static let shared = NetworkManager()
     
+    private let api = "https://swiftbook.ru//wp-content/uploads/api/api_courses"
+    
     private init() {}
     
-    func fetchData() {
+    func fetchData(completion: @escaping ([Course]) -> Void) {
+        guard let url = URL(string: api) else { return }
         
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let courses = try decoder.decode([Course].self, from: data)
+                completion(courses)
+            } catch {
+                print(error)
+            }
+        }.resume()
     }
     
-    func fetchImage() {
+    func fetchImage(from url: URL?) -> Data? {
+        guard let url = url else { return nil }
+        guard let imageData = try? Data(contentsOf: url) else { return nil }
         
+        return imageData
     }
 }
